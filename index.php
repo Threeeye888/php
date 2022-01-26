fc5c8af98d6da871406cf594c5ae95bc1c1108ab
Threeeye888 <tysonfraser6x5@gmail.com>
2022-01-25 19:15:51 -0700

    test

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title></title>
+</head>
+<body>
+
+ <main>
+
+<p>Send Email</p>
+<form class="contact-form" action="contactform.php" method="post">
+ <input type="text" name="name" placeholder="Full Name">
+ <input type="text" name="mail" placeholder="Email">
+ <input type="text" name="subject" placeholder="Subject">
+ <textarea name="message" placeholder="Message"></textarea>
+ <button type="submit" name="submit">Send Email</button>	
+
+</form>
+
+ </main>
+
+</body>
+</html>
